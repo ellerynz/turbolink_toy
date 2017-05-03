a660517ba83c027fc22240c15c1aac4329605571
ellerynz <hello@ellerynz.com>
2017-05-03 10:59:09 -0500

    Add dynamic item creation

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,25 @@
 class ItemsController < ApplicationController
 
   def index
+    @items = Item.all
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+
+    respond_to do |format|
+      if @item.save
+        format.js
+        format.json { render json: @item, status: :created, location: @item }
+      end
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:value)
   end
 
 end
